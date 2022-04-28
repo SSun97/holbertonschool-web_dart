a73548daf88399cd70d49fc662f56a739e6adc03
ssun97 <sunmingyu.97@gmail.com>
2022-04-27 21:31:00 -0500

    use loop

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,7 +1,8 @@
 List<double> convertToF(List<double> temperaturesInC) {
     List<double> temperaturesInF = [];
+    // print((temperaturesInC[0] * 1.8 + 32).runtimeType);
     for (var i = 0; i < temperaturesInC.length; i++) {
-        temperaturesInF.add( (temperaturesInC[i] * 1.8) + 32 );
+        temperaturesInF.add(temperaturesInC[i] * 1.8 + 32);
     }
     return temperaturesInF;
 }
